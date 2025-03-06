769a54e23d8a7341dcad0ff709da0e27dedd6668
razendra17 <daffaevandrez25@gmail.com>
2025-03-06 13:09:02 +0800

    update

--- proper.js
+++ proper.js
@@ -8,3 +8,13 @@ onclick = () => {
     navbarnavv.classList.toggle('active');
     
 };
+
+document.getElementById("search-icon").addEventListener("click",function() {
+        let searchInput = document.getElementById("search-input");
+        searchInput.classList.toggle("active")
+        if (searchInput.classList.contains("active")) {
+            searchInput.focus(); // Fokus ke input setelah muncul
+        } else {
+            searchInput.blur(); // Hilangkan fokus saat disembunyikan
+        }
+    })
